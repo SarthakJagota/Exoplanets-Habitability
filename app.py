47a736d741fb0abcb07811181e9e7fdcb1f996d3
Sarthak Jagota <sarthakjagota34@gmail.com>
2026-02-16 23:47:09 +0530

    Remove pip install for streamlit from app.py
    Removed pip install command for streamlit.

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1yd43FDYkiOhKi3NqCr0ybzO-Yps5JDly
 """
 
-!pip install streamlit
 
 import streamlit as st
 import numpy as np
